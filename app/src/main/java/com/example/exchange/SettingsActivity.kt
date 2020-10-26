4ca159505cb6ccc08a9c100ffc281e3a342cd2d3
Volhamster <odkotova@yandex.ru>
2020-10-26 16:44:02 +0300

    not final nav graph

--- app/src/main/java/com/example/exchange/SettingsActivity.kt
+++ app/src/main/java/com/example/exchange/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.exchange
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.*
